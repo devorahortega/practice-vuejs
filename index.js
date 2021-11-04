35ffeee750d0c9e19695a72ab3a2f896a2c4db35
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-04 12:51:31 -0400

    Adds button to page

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ var app = new Vue({
     };
   },
 });
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Hello Vue.js!",
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
