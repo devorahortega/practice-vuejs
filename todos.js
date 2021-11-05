25ab3d6ae2c46485221b6884a99472a3ca084dc2
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-05 13:57:59 -0400

    Adds list of todos and button

--- todos.js
+++ todos.js
@@ -1,33 +1,20 @@
-/* global Vue */
+/* global Vue , axios*/
 
 var app = new Vue({
   el: "#app",
   data: function () {
     return {
       message: "Hello from JavaScript!",
-      todos: [
-        {
-          userId: 1,
-          id: 1,
-          title: "Do the things",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: "Do the other things",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "Do the more things",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
-    loadTodos: function () {},
+    // ...
+    loadTodos: function () {
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      });
+    },
   },
 });
